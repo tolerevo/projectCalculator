25cfc0a71d92bf8698728fa57f638f0dafde4286
Diego Tolentino <114973390+tolerevo@users.noreply.github.com>
2024-08-02 15:05:38 -0600

    Do html landing page and linked it  to css file

---
+++ script.js
@@ -0,0 +1,19 @@
+const a = 0;
+const b = 0;
+const operator = ""
+
+const operate = (a, b, operator) => {
+    if (operator === "+") {
+        return a + b;
+    } else if (operator === "-") {
+        return a - b;
+    } else if (operator === "*") {
+        return a * b;
+    } else if (operator === "/") {
+        return a / b;
+    } else {
+        return "Error";
+    }
+}
+console.log(operate(2, 3, "+"));
+
